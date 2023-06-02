beb32b57f7a16825b297194e84f91f5ba7dbfd5e
unknown <e.geramizadeh13821359@gmail.com>
2023-06-02 11:28:46 +0330

    some features added and an error fixed

--- graph.py
+++ graph.py
@@ -1,6 +1,7 @@
 class graph():
     def __init__(self,initial_dic={}):
         self.network=initial_dic
+        self.colors={}
     def AddNode(self,node):
         self.network[node]=set({})
 
@@ -10,7 +11,7 @@ class graph():
         
     def DeleteNode(self,node):
         for i in self.network[node]:
-            self.network[i].remove[node]
+            self.network[i].remove(node)
         self.network.pop(node)
 
     def DeleteEdge(self,node1,node2):
@@ -25,18 +26,53 @@ class graph():
     def GetConnections(self,node):
         '''returns a set that contains the nodes that the given node is connected to'''
         return self.network[node]
-                
-  
+    def Color(self,node,positive_color):
+        '''note that the color must be a positive number'''
+        self.colors[node]=positive_color
 
+    def IsColored(self,node):
+        return (node in self.colors)
+    def GetColor(self,node):
+        return self.colors[node]
+    
+    def FindPath(self,start,dest):
+        '''finds a path betwean start and des and returns it as a list'''
+        path=self._FindPath(start,dest)
+        size=len(path);
+        for i in range(size//2):
+            temp=path[i]
+            path[i]=path[size-1-i]
+            path[size-1-i]=temp
+        return path
 
-############################test
-# gr=graph()
-# gr.AddNode(1)
-# gr.AddNode(2)
-# gr.AddNode(3)
-# gr.AddEdge(2,3);
-# gr.AddEdge(1,2);
-# print(gr.network);
-# print(gr.IsThere(1))
-# print(gr.GetConnections(1))
-###############################
+    def _FindPath(self,curr,dest):
+        '''use FindPath instead to find a path'''
+        if(curr==dest):
+                return [dest]
+        self.Color(curr,1)
+        for node in self.network[curr]:
+            if(not self.IsColored(node)):
+                founded_path=self._FindPath(node,dest)
+                if(len(founded_path)!=0):
+                    founded_path.append(curr)
+                    return founded_path
+        return []
+            
+               
+    
+######################test
+#gr=graph();
+#gr.AddNode(1);
+#gr.AddNode(2);
+#gr.AddNode(3);
+#gr.AddNode(4);
+#gr.AddNode(5);
+#gr.AddNode(6);
+#gr.AddEdge(1,2);
+#gr.AddEdge(2,3);
+#gr.AddEdge(2,5);
+#gr.AddEdge(3,6);
+#gr.AddEdge(4,5);
+#print(gr.FindPath(1,5));
+###########################
+
