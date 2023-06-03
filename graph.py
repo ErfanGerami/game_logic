096f1dbb36323736b655065f1e5f93388b3719c8
ErfanGerami <52499982+ErfanGerami@users.noreply.github.com>
2023-06-03 06:58:38 +0330

    added some features and fixed an error

--- graph.py
+++ graph.py
@@ -1,6 +1,9 @@
 class graph():
     def __init__(self,initial_dic={}):
         self.network=initial_dic
+        self.colors=[]
+        self.min_path=-1
+        self.path=[]
     def AddNode(self,node):
         self.network[node]=set({})
 
@@ -10,7 +13,7 @@ class graph():
         
     def DeleteNode(self,node):
         for i in self.network[node]:
-            self.network[i].remove[node]
+            self.network[i].remove(node)
         self.network.pop(node)
 
     def DeleteEdge(self,node1,node2):
@@ -19,24 +22,80 @@ class graph():
     #-------------------------------------
     def IsThere(self,node):
         '''return True if the node is in the graph and False if it is not'''
-        if node in self.network:
-            return True
-        return False
+        return (node in self.network)
     def GetConnections(self,node):
         '''returns a set that contains the nodes that the given node is connected to'''
         return self.network[node]
-                
+    
+    #---------------------------------------------
+    def StartColoring(self):
+        '''use to reset or start coloring'''
+        maximum_node=max(self.network)
+        self.colors=[]
+        for i in range(0,maximum_node+1):
+            self.colors.append(0)
+
+    def Color(self,node,color):
+        '''note that the color must be a positive number'''
+        self.colors[node]=color
+
+    def GetColor(self,node):
+        return self.colors[node]
+    
+    def FindPath(self,start,dest):
+        '''finds a path betwean start and des and returns it as a list'''
+        self.min_path=len(self.network)+1
+        colors_temp=self.colors.copy()
+        self.StartColoring()
+        self.path=[]
+
+        self._FindPath(start,dest,0,[])
+        self.colors=colors_temp.copy()
+        return self.path
+
+    def _FindPath(self,curr,dest,depth,curr_path):
+        '''use FindPath instead to find a path'''
+        curr_path.append(curr)
+        if(depth>=self.min_path):
+            return 
+            
+        if(curr==dest):
+            self.min=depth
+            self.path=curr_path.copy()
+
+        self.Color(curr,depth)
+        for node in self.network[curr]:
+
+            if(self.GetColor(node)>depth+1 or self.GetColor(node)==0):
+                self.Color(node,depth+1)
+                self._FindPath(node,dest,depth+1,curr_path)
+                curr_path.pop()
+        
+            
+               
   
 
+if __name__=="__main__":
+    gr=graph()
+    gr.AddNode(1)
+    
+    gr.AddNode(2)
+    gr.AddNode(3)
+    gr.AddNode(4)
+    gr.AddNode(5)
+    gr.AddNode(6)
+    gr.AddNode(7)
+    gr.DeleteNode(7)
 
-############################test
-# gr=graph()
-# gr.AddNode(1)
-# gr.AddNode(2)
-# gr.AddNode(3)
-# gr.AddEdge(2,3);
-# gr.AddEdge(1,2);
-# print(gr.network);
-# print(gr.IsThere(1))
-# print(gr.GetConnections(1))
-###############################
+    gr.AddEdge(1,2)
+    gr.AddEdge(2,3)
+    gr.AddEdge(2,5)
+    gr.AddEdge(3,6)
+    gr.AddEdge(4,5)
+    gr.AddEdge(1,6)
+    gr.DeleteEdge(1,6)
+    print(gr.network)
+    print(gr.FindPath(1,5))
+    print(gr.FindPath(1,6))
+    
+
